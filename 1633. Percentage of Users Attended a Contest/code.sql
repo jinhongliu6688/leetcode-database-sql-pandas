5fe6b069c134c377e42ce6c09b371b02dbbb33de
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-05 21:48:07 -0800

    Create code.sql

---
+++ 1633. Percentage of Users Attended a Contest/code.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT
+    contest_id,
+    ROUND(COUNT(*) / (SELECT COUNT(user_id) FROM Users) * 100, 2) AS percentage
+FROM
+    Register
+GROUP BY
+    contest_id
+ORDER BY
+    percentage DESC,
+    contest_id ASC;
