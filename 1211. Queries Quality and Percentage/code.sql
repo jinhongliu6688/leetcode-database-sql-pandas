2ad5561d329ada2e22fd5cba7fb07c5a4f598b6d
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-06 16:47:20 -0800

    Update code.sql

--- 1211. Queries Quality and Percentage/code.sql
+++ 1211. Queries Quality and Percentage/code.sql
@@ -10,3 +10,16 @@ WHERE
     query_name IS NOT NULL
 GROUP BY
     query_name;
+
+# Write your MySQL query statement below
+
+SELECT
+    query_name,
+    ROUND(SUM(rating / position) / COUNT(rating / position), 2) AS quality,
+    ROUND(COUNT(CASE WHEN rating < 3 THEN rating END)/COUNT(rating) * 100, 2) AS poor_query_percentage
+FROM
+    Queries
+WHERE
+    query_name IS NOT NULL
+GROUP BY
+    query_name;
