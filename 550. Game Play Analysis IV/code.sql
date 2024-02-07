3c9be6542c38d9ec46b281716c4629f73f6fd67b
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-06 22:54:10 -0800

    Create code.sql

---
+++ 550. Game Play Analysis IV/code.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+SELECT
+    ROUND(COUNT(DISTINCT A1.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM
+    (SELECT
+        player_id,
+        MIN(event_date) AS first_login_date
+    FROM
+        Activity
+    GROUP BY
+        player_id) A1
+INNER JOIN
+    Activity A2
+ON
+    DATEDIFF(A1.first_login_date, A2.event_date) = -1 AND A1.player_id = A2.player_id;
