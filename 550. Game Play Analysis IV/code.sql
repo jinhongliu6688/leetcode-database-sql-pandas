e5244ed504c570a521ffc3a5faf4e881ba507093
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-06 22:57:40 -0800

    Update code.sql

--- 550. Game Play Analysis IV/code.sql
+++ 550. Game Play Analysis IV/code.sql
@@ -14,3 +14,22 @@ INNER JOIN
     Activity A2
 ON
     DATEDIFF(A1.first_login_date, A2.event_date) = -1 AND A1.player_id = A2.player_id;
+
+
+
+# Write your MySQL query statement below
+
+SELECT
+    ROUND(AVG(A2.event_date IS NOT NULL), 2) AS fraction
+FROM
+    (SELECT
+        player_id,
+        MIN(event_date) AS first_login_date
+    FROM
+        Activity
+    GROUP BY
+        player_id) A1
+LEFT JOIN
+    Activity A2
+ON
+    DATEDIFF(A1.first_login_date, A2.event_date) = -1 AND A1.player_id = A2.player_id;
