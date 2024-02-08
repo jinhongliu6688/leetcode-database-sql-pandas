be084036dbd5aff4e611f60c6870d922bbd60cdb
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-07 17:17:34 -0800

    Update code.sql

--- 619. Biggest Single Number/code.sql
+++ 619. Biggest Single Number/code.sql
@@ -12,3 +12,32 @@ FROM (
     HAVING 
         COUNT(num) = 1
 ) table1
+
+# Write your MySQL query statement below
+
+SELECT (
+    SELECT
+        num
+    FROM
+        MyNumbers
+    GROUP BY
+        num
+    HAVING 
+        COUNT(num) = 1
+    ORDER BY
+        num DESC
+    LIMIT
+        1
+) num
+
+# Write your MySQL query statement below
+
+SELECT
+    IFNULL(
+        (SELECT num
+         FROM MyNumbers
+         GROUP BY num
+         HAVING COUNT(num) = 1
+         ORDER BY num DESC
+         LIMIT 1),
+    NULL) AS num
