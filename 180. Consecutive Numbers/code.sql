4fba96b09cf9a8ac1a87274c86faf4f30716ae6b
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-07 22:59:08 -0800

    Create code.sql

---
+++ 180. Consecutive Numbers/code.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+SELECT
+    DISTINCT L1.num ConsecutiveNums
+FROM
+    Logs L1
+INNER JOIN
+    Logs L2
+ON
+    L1.id + 1 = L2.id AND L1.num = L2.num
+INNER JOIN
+    Logs L3
+ON
+    L2.id + 1 = L3.id AND L2.num = L3.num
