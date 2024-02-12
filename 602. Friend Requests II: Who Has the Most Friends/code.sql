27e8d304d55ec18d75ee210c4f82296f140e1d0a
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-11 17:34:05 -0800

    Create code.sql

---
+++ 602. Friend Requests II: Who Has the Most Friends/code.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+SELECT
+    id,
+    COUNT(*) AS num
+FROM
+    (SELECT
+        requester_id AS id
+    FROM
+        RequestAccepted
+
+    UNION ALL
+
+    SELECT
+        accepter_id AS id
+    FROM
+        RequestAccepted) AS table_1
+GROUP BY
+    id
+ORDER BY
+    num DESC
+LIMIT
+    1
