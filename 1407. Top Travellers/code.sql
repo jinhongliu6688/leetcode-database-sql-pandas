e651c4e3b1e4597cd29c53f344824c9026bb2cdc
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-23 19:44:47 -0800

    Create code.sql

---
+++ 1407. Top Travellers/code.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+SELECT
+    U.name,
+    IFNULL(SUM(distance), 0) AS travelled_distance 
+FROM
+    Users U
+LEFT JOIN
+    Rides R ON U.id = R.user_id
+GROUP BY
+    U.id
+ORDER BY
+    travelled_distance DESC,
+    U.name ASC
