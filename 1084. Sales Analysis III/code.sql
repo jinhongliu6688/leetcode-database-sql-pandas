d300150cf900f1e7bbd82aa0cc393e848e8e4f96
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-23 18:51:11 -0800

    Create code.sql

---
+++ 1084. Sales Analysis III/code.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+SELECT
+    product_id,
+    product_name
+FROM
+    Product
+WHERE
+    product_id NOT IN (
+        SELECT
+            P.product_id
+        FROM
+            Product P
+        INNER JOIN
+            Sales S ON P.product_id = S.product_id 
+        WHERE
+            sale_date NOT BETWEEN "2019-01-01" AND "2019-03-31"
+    ) AND product_id IN (
+        SELECT
+            product_id
+        FROM
+            Sales
+    )
