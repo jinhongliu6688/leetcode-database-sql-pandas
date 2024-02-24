95d773c3235d8dd14f22971a6fd05798a411abc1
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-23 18:55:26 -0800

    Update code.sql

--- 1084. Sales Analysis III/code.sql
+++ 1084. Sales Analysis III/code.sql
@@ -21,3 +21,17 @@ WHERE
         FROM
             Sales
     )
+
+# Write your MySQL query statement below
+
+SELECT
+    P.product_id,
+    P.product_name
+FROM
+    Product P
+INNER JOIN
+    Sales S ON P.product_id = S.product_id 
+GROUP BY
+    P.product_id
+HAVING
+    SUM(IF(S.sale_date BETWEEN "2019-01-01" AND "2019-03-31", 0, 1)) = 0
