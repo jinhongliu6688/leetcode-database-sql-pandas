2d524135c241fa7d0a896a7bc90b45fff21f210f
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-24 18:17:03 -0800

    Update code.sql

--- 597. Friend Requests I: Overall Acceptance Rate/code.sql
+++ 597. Friend Requests I: Overall Acceptance Rate/code.sql
@@ -9,3 +9,13 @@ SELECT
         ), 0), 2) AS accept_rate
 FROM
     RequestAccepted
+
+SELECT (
+    ROUND(
+        IFNULL(
+                (SELECT COUNT(DISTINCT requester_id, accepter_id) FROM RequestAccepted)
+                /
+                (SELECT COUNT(DISTINCT sender_id, send_to_id) FROM FriendRequest)
+        , 0)
+    , 2)
+) AS accept_rate
