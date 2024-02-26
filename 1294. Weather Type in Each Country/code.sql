f1ec32992241c213f7460c86d67700f2b9606351
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-25 22:44:43 -0800

    Create code.sql

---
+++ 1294. Weather Type in Each Country/code.sql
@@ -0,0 +1,39 @@
+# Write your MySQL query statement below
+
+SELECT
+    country_name,
+    CASE
+        WHEN AVG(weather_state) <= 15 THEN "Cold"
+        WHEN AVG(weather_state) >= 25 THEN "Hot"
+        ELSE "Warm"
+    END AS weather_type
+FROM
+    Countries c
+INNER JOIN
+    Weather w
+ON
+    c.country_id = w.country_id
+WHERE
+    day BETWEEN "2019-11-01" AND "2019-11-30"
+GROUP BY
+    c.country_name
+
+# Write your MySQL query statement below
+
+SELECT
+    country_name,
+    CASE
+        WHEN AVG(weather_state) <= 15 THEN "Cold"
+        WHEN AVG(weather_state) >= 25 THEN "Hot"
+        ELSE "Warm"
+    END AS weather_type
+FROM
+    Countries c
+INNER JOIN
+    Weather w
+ON
+    c.country_id = w.country_id
+WHERE
+    day BETWEEN "2019-11-01" AND "2019-11-30"
+GROUP BY
+    c.country_id
