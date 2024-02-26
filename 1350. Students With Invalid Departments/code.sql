b3b24d3f04185168f49d70ecb97584c42a2557bd
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-26 00:00:41 -0800

    Update code.sql

--- 1350. Students With Invalid Departments/code.sql
+++ 1350. Students With Invalid Departments/code.sql
@@ -7,3 +7,19 @@ FROM
     Students
 WHERE
     department_id NOT IN (SELECT id FROM Departments)
+
+# Write your MySQL query statement below
+
+SELECT
+    id,
+    name
+FROM
+	Students AS s
+WHERE NOT EXISTS(
+	SELECT
+		1
+	FROM
+		Departments
+	WHERE
+		id = s.department_id
+);
