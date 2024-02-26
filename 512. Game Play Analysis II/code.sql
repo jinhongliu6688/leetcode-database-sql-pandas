6cb64608c526595a483677eec6268f035023b576
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-25 20:30:40 -0800

    Update code.sql

--- 512. Game Play Analysis II/code.sql
+++ 512. Game Play Analysis II/code.sql
@@ -16,3 +16,20 @@ WHERE
             GROUP BY
                 player_id
         )
+
+# Write your MySQL query statement below
+
+SELECT
+    player_id,
+    device_id
+FROM
+    (
+        SELECT
+            player_id,
+            device_id,
+            RANK() OVER (PARTITION BY player_id ORDER BY event_date ASC) AS ranking
+        FROM
+            Activity
+    ) AS table_1
+WHERE
+    ranking = 1
