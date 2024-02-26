c5f2e4af41c851443aa007263f3dd04c739c6c27
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-25 21:24:30 -0800

    Update code.sql

--- 1142. User Activity for the Past 30 Days II/code.sql
+++ 1142. User Activity for the Past 30 Days II/code.sql
@@ -14,3 +14,12 @@ FROM
     GROUP BY
         user_id
 ) AS table_1
+
+# Write your MySQL query statement below
+
+SELECT
+    IFNULL(ROUND(COUNT(DISTINCT session_id) / COUNT(DISTINCT user_id), 2), 0) AS average_sessions_per_user
+FROM
+    Activity
+WHERE
+    DATEDIFF("2019-07-27", activity_date) <= 29
