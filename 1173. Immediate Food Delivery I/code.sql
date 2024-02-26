fc126fa70a31776084b0b5cdf82fec35bc81d701
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-25 21:42:16 -0800

    Update code.sql

--- 1173. Immediate Food Delivery I/code.sql
+++ 1173. Immediate Food Delivery I/code.sql
@@ -4,3 +4,12 @@ SELECT
     ROUND(SUM(IF(customer_pref_delivery_date = order_date, 1, 0)) / COUNT(*) * 100, 2) AS immediate_percentage
 FROM
     Delivery
+
+# Write your MySQL query statement below
+
+SELECT
+    ROUND(
+        AVG(customer_pref_delivery_date = order_date) * 100
+    , 2) AS immediate_percentage
+FROM
+    Delivery
