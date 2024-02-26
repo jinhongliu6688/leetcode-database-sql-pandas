770331abc870712892fba0a1b3fa00dadfc7e0da
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-25 20:54:38 -0800

    Create code.sql

---
+++ 1113. Reported Posts/code.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+SELECT
+    extra AS report_reason,
+    COUNT(DISTINCT post_id) AS report_count
+FROM
+    Actions
+WHERE
+    extra IS NOT NULL AND
+    action_date = "2019-07-04" AND
+    action = "report"
+GROUP BY
+    extra
