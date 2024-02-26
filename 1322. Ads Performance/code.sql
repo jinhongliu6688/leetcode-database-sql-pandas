c02bdf79dc60a19fb5f1a08ce8b240136efa3614
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-25 23:38:23 -0800

    Update code.sql

--- 1322. Ads Performance/code.sql
+++ 1322. Ads Performance/code.sql
@@ -10,3 +10,23 @@ GROUP BY
 ORDER BY
     ctr DESC,
     ad_id ASC
+
+# Write your MySQL query statement below
+
+SELECT
+    ad_id,
+    IFNULL(
+        ROUND(
+            SUM(action = "Clicked") 
+            / 
+            (SUM(action = "Clicked") + SUM(action = "Viewed"))
+            * 100
+        , 2)
+    , 0) AS ctr
+FROM
+    Ads
+GROUP BY
+    ad_id
+ORDER BY
+    ctr DESC,
+    ad_id ASC
