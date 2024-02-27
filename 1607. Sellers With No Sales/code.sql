2401892d9d6070ec6069064d7a71f304ca9396dc
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-27 00:25:04 -0800

    Update code.sql

--- 1607. Sellers With No Sales/code.sql
+++ 1607. Sellers With No Sales/code.sql
@@ -15,3 +15,19 @@ WHERE
     )
 ORDER BY
     seller_name
+
+# Write your MySQL query statement below
+
+SELECT
+    seller_name
+FROM
+    Seller s
+LEFT JOIN
+    Orders o
+ON 
+    s.seller_id = o.seller_id AND
+    sale_date LIKE "2020%"
+WHERE
+    order_id IS NULL
+ORDER BY
+    seller_name
