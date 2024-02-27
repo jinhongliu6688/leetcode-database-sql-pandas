6e5db0cf86d9f2d556d5a9805c8cbf6efc53c7d5
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-26 23:34:16 -0800

    Update code.sql

--- 1543. Fix Product Name Format/code.sql
+++ 1543. Fix Product Name Format/code.sql
@@ -19,3 +19,18 @@ GROUP BY
 ORDER BY
     product_name ASC,
     sale_date ASC
+
+# Write your MySQL query statement below
+
+SELECT
+    LOWER(TRIM(product_name)) AS product_name,
+    LEFT(sale_date, 7) AS sale_date,
+    COUNT(*) AS total
+FROM 
+    Sales
+GROUP BY
+    LOWER(TRIM(product_name)),
+    LEFT(sale_date, 7)
+ORDER BY
+    product_name ASC,
+    sale_date ASC
