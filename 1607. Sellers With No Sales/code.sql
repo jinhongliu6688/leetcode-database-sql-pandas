6d631b0403a03c47a634f4eb3049a8d3343d1bb0
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-27 00:11:57 -0800

    Create code.sql

---
+++ 1607. Sellers With No Sales/code.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+SELECT
+    seller_name
+FROM
+    Seller
+WHERE
+    seller_id NOT IN (
+        SELECT
+            seller_id
+        FROM
+            Orders
+        WHERE
+            sale_date LIKE "2020%"
+    )
+ORDER BY
+    seller_name
