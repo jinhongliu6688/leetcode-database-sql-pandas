fc65689a2bdb87eed336333090879ea02a46c8e9
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-28 03:52:22 -0800

    Update code.sql

--- 1965. Employees With Missing Information/code.sql
+++ 1965. Employees With Missing Information/code.sql
@@ -29,3 +29,25 @@ FROM(
 ) AS temp
 ORDER BY
     employee_id
+
+# Write your MySQL query statement below
+
+SELECT
+    employee_id
+FROM (
+    SELECT
+        employee_id
+    FROM
+        Employees
+    UNION ALL
+    SELECT
+        employee_id
+    FROM
+        Salaries
+) temp
+GROUP BY
+    employee_id
+HAVING
+    COUNT(employee_id) = 1
+ORDER BY
+    employee_id
