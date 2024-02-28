5318bb09863a13523cb64aebf56af082c5618de5
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-28 02:52:19 -0800

    Update code.sql

--- 1853. Convert Date Format/code.sql
+++ 1853. Convert Date Format/code.sql
@@ -4,3 +4,10 @@ SELECT
     CONCAT(DAYNAME(day), ", ", DATE_FORMAT(day, "%M %e"), ", ", YEAR(day)) AS day
 FROM
     Days
+
+# Write your MySQL query statement below
+
+SELECT
+    DATE_FORMAT(day, "%W, %M %e, %Y") AS day
+FROM
+    Days
