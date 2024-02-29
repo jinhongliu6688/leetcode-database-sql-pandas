6c4074acce6fee468f6a4384ca9b993c320d83a7
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-28 21:30:19 -0800

    Update code.sql

--- 2082. The Number of Rich Customers/code.sql
+++ 2082. The Number of Rich Customers/code.sql
@@ -12,3 +12,12 @@ FROM (
     HAVING
         MAX(amount) > 500
 ) AS temp
+
+# Write your MySQL query statement below
+
+SELECT
+    COUNT(DISTINCT customer_id) AS rich_count
+FROM
+    store
+WHERE
+    amount > 500
