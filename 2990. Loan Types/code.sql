585f1d2dcfcce56edc96701c0c95c9121ee4924d
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-29 01:35:36 -0800

    Update code.sql

--- 2990. Loan Types/code.sql
+++ 2990. Loan Types/code.sql
@@ -11,3 +11,19 @@ HAVING
     GROUP_CONCAT(loan_type) LIKE "%Mortgage%"
 ORDER BY
     user_id
+
+# Write your MySQL query statement below
+
+SELECT
+    user_id
+FROM
+    Loans
+WHERE
+    loan_type = "Refinance" OR
+    loan_type = "Mortgage"
+GROUP BY
+    user_id
+HAVING
+    COUNT(DISTINCT user_id, loan_type) = 2
+ORDER BY
+    user_id
