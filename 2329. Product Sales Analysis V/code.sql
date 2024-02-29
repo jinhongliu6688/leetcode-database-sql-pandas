2320e19ee7558e8b7f783ccf16e5da34388ca2d2
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-28 23:07:53 -0800

    Create code.sql

---
+++ 2329. Product Sales Analysis V/code.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+SELECT
+    user_id,
+    SUM(quantity * price) AS spending
+FROM
+    Sales s
+INNER JOIN
+    Product p
+ON
+    s.product_id = p.product_id
+GROUP BY
+    user_id
+ORDER BY
+    spending DESC,
+    user_id ASC
