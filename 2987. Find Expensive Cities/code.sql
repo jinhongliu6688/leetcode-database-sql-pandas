b31eecdb738d68837232b7072da2d517ee14693b
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-29 01:21:16 -0800

    Create code.sql

---
+++ 2987. Find Expensive Cities/code.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+SELECT
+    city
+FROM
+    Listings
+GROUP BY
+    city
+HAVING
+    AVG(price) > (
+        SELECT
+            AVG(price)
+        FROM
+            Listings
+    )
+ORDER BY
+    city
