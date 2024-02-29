a9d5c1fd8abc2f976797a2a41095d1fa88ae9bad
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-02-29 00:45:39 -0800

    Create code.sql

---
+++ 2853. Highest Salaries Difference/code.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+
+SELECT
+    ABS((
+        SELECT
+            MAX(salary)
+        FROM
+            Salaries
+        WHERE
+            department = "Engineering"
+    ) 
+    - 
+    (
+        SELECT
+            MAX(salary)
+        FROM
+            Salaries
+        WHERE
+            department = "Marketing"
+    )) AS salary_difference
