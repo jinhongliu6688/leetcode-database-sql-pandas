8617d8f444d52554b821ca2405b01d3c2319e78c
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-01 00:55:00 -0800

    Update code.sql

--- 1126. Active Businesses/code.sql
+++ 1126. Active Businesses/code.sql
@@ -1,19 +1,6 @@
 # Write your MySQL query statement below
 
-WITH business AS (
-    SELECT
-        business_id,
-        event_type,
-        AVG(occurrences) average
-    FROM
-        Events
-    GROUP BY
-        business_id,
-        event_type
-    ORDER BY
-        business_id
-),
-type_avg AS (
+WITH type_avg AS (
     SELECT
         event_type,
         AVG(occurrences) average
@@ -25,12 +12,12 @@ type_avg AS (
 SELECT
     business_id
 FROM
-    business b
+    Events e
 INNER JOIN
     type_avg t
 ON
-    b.event_type = t.event_type
+    e.event_type = t.event_type
 GROUP BY
     business_id
 HAVING
-    SUM(IF(b.average > t.average, 1, 0)) > 1
+    SUM(IF(e.occurrences > t.average, 1, 0)) > 1
