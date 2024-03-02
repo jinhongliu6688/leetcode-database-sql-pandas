f8edaeed18acdb13304135b00354417e13ebac0b
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-02 03:16:45 -0800

    Create code.sql

---
+++ 1264. Page Recommendations/code.sql
@@ -0,0 +1,31 @@
+# Write your MySQL query statement below
+
+SELECT
+    DISTINCT page_id recommended_page
+FROM
+    Likes
+WHERE
+    user_id IN (
+        SELECT
+            user2_id
+        FROM
+            Friendship
+        WHERE
+            user1_id = 1
+
+        UNION
+
+        SELECT
+            user1_id
+        FROM
+            Friendship
+        WHERE
+            user2_id = 1
+    ) AND page_id NOT IN (
+        SELECT
+            page_id
+        FROM
+            Likes
+        WHERE
+            user_id = 1
+    )
