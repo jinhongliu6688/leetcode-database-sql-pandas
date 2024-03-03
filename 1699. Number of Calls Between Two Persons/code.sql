e7867f214f8fde11d1293a471d09eb4cbb9df669
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-02 23:48:26 -0800

    Update code.sql

--- 1699. Number of Calls Between Two Persons/code.sql
+++ 1699. Number of Calls Between Two Persons/code.sql
@@ -16,3 +16,22 @@ FROM (
 GROUP BY
     from_id,
     to_id
+
+# Write your MySQL query statement below
+
+SELECT
+    from_id person1,
+    to_id person2,
+    COUNT(duration) call_count,
+    SUM(duration) total_duration
+FROM (
+    SELECT
+        LEAST(from_id, to_id) from_id,
+        GREATEST(from_id, to_id) to_id,
+        duration
+    FROM
+        Calls 
+) temp
+GROUP BY
+    from_id,
+    to_id
