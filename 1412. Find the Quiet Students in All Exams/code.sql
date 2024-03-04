8f5f58d0c376168449f84a12cee8b03c1c496e03
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-03 21:06:13 -0800

    Update code.sql

--- 1412. Find the Quiet Students in All Exams/code.sql
+++ 1412. Find the Quiet Students in All Exams/code.sql
@@ -46,3 +46,35 @@ WHERE
     )
 ORDER BY
     student_id
+
+# Write your MySQL query statement below
+
+SELECT
+    DISTINCT student_id,
+    student_name
+FROM
+    Student s
+INNER JOIN
+    Exam e
+USING(student_id)
+WHERE
+    student_id NOT IN (
+        SELECT
+            student_id
+        FROM(
+            SELECT
+                student_id,
+                rank() over(partition by exam_id order by score desc) hight_to_low_rank,
+                rank() over(partition by exam_id order by score asc) low_to_high_rank
+            FROM
+                Student s
+            INNER JOIN
+                Exam e
+            USING(student_id)
+        ) temp
+        WHERE
+            hight_to_low_rank = 1 OR
+            low_to_high_rank = 1
+    )
+ORDER BY
+    student_id
