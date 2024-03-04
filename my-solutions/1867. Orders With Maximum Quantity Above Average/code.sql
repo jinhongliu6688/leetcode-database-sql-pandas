1a48ad3aaac0ae6e9a6b31a937a06417704056e5
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-04 09:19:17 -0800

    Create code.sql

---
+++ my-solutions/1867. Orders With Maximum Quantity Above Average/code.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+SELECT
+    order_id
+FROM
+    OrdersDetails
+GROUP BY
+    order_id
+HAVING
+    MAX(quantity) > ALL (
+        SELECT
+            AVG(quantity)
+        FROM
+            OrdersDetails
+        GROUP BY
+            order_id
+    )
