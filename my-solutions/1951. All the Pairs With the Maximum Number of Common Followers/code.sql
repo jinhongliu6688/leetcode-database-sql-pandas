7bab78b8788d3aa42fab6d922e8a6697b5f4a9ad
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-04 13:27:46 -0800

    Update code.sql

--- my-solutions/1951. All the Pairs With the Maximum Number of Common Followers/code.sql
+++ my-solutions/1951. All the Pairs With the Maximum Number of Common Followers/code.sql
@@ -22,3 +22,36 @@ FROM (
 ) temp
 WHERE
     ranking = 1
+
+# Write your MySQL query statement below
+
+WITH cte AS (
+    SELECT
+        r1.user_id user1_id,
+        r2.user_id user2_id,
+        COUNT(*) count_val
+    FROM
+        Relations r1
+    INNER JOIN
+        Relations r2
+    USING
+        (follower_id)
+    WHERE
+        r1.user_id < r2.user_id
+    GROUP BY
+        r1.user_id,
+        r2.user_id
+)
+
+SELECT
+    user1_id,
+    user2_id
+FROM
+    cte
+WHERE
+    count_val = (
+        SELECT
+            MAX(count_val)
+        FROM
+            cte
+    )
