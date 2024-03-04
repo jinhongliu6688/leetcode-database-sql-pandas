c3532eeed831f20f9c82a35685ed68fb756a201b
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-04 08:05:44 -0800

    Update code.sql

--- my-solutions/1843. Suspicious Bank Accounts/code.sql
+++ my-solutions/1843. Suspicious Bank Accounts/code.sql
@@ -27,3 +27,33 @@ GROUP BY
     tag
 HAVING
     COUNT(*) >= 2
+
+# Write your MySQL query statement below
+
+SELECT
+    distinct account_id
+FROM (
+    SELECT
+        account_id,
+        LEFT(day, 7) month,
+        SUM(amount) total_amount,
+        max_income,
+        CONCAT(account_id, "-", period_diff(DATE_FORMAT(day, "%Y%m"), "200001") - rank() over(partition by account_id order by LEFT(day, 7))) as tag
+    FROM
+        Transactions t
+    INNER JOIN
+        Accounts a
+    USING
+        (account_id)
+    WHERE
+        type = "Creditor"
+    GROUP BY
+        account_id,
+        LEFT(day, 7)
+    HAVING
+        SUM(amount) > max_income
+) temp
+GROUP BY
+    tag
+HAVING
+    COUNT(*) >= 2
