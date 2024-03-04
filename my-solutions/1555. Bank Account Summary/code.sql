b60167172a372555f1e96ba0e3050c966f8cc20e
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-04 02:50:27 -0800

    Update code.sql

--- my-solutions/1555. Bank Account Summary/code.sql
+++ my-solutions/1555. Bank Account Summary/code.sql
@@ -29,3 +29,20 @@ LEFT JOIN (
 ) temp_2
 ON
     u.user_id = temp_2.paid_to
+
+# Write your MySQL query statement below
+
+SELECT
+    user_id,
+    user_name,
+    AVG(credit) + IFNULL(SUM(IF(user_id = paid_to, 1, -1) * amount), 0) credit,
+    IF(AVG(credit) + IFNULL(SUM(IF(user_id = paid_to, 1, -1) * amount), 0) < 0, "Yes", "No") credit_limit_breached
+FROM
+    Users u 
+LEFT JOIN
+    Transactions t
+ON
+    u.user_id = t.paid_by OR
+    u.user_id = t.paid_to
+GROUP BY
+    user_id
