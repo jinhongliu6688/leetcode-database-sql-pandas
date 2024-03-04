c0438bcf9e1899a19dbe22d50bb799d4dfb4d4a3
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-04 09:34:25 -0800

    Update code.sql

--- my-solutions/1867. Orders With Maximum Quantity Above Average/code.sql
+++ my-solutions/1867. Orders With Maximum Quantity Above Average/code.sql
@@ -15,3 +15,17 @@ HAVING
         GROUP BY
             order_id
     )
+
+# Write your MySQL query statement below
+
+SELECT
+    order_id
+FROM (
+    SELECT
+        order_id,
+        MAX(quantity) > MAX(AVG(quantity)) OVER() AS flag
+    FROM OrdersDetails
+    GROUP BY order_id
+) temp
+WHERE 
+    flag = 1
