956825975b963cf05ffd56ccb8d5718344a46275
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-05 06:08:11 -0800

    Update code.sql

--- my-solutions/2020. Number of Accounts That Did Not Stream/code.sql
+++ my-solutions/2020. Number of Accounts That Did Not Stream/code.sql
@@ -9,6 +9,20 @@ INNER JOIN
 USING
     (account_id)
 WHERE
-    (year(start_date) = "2021" OR
-    year(end_date) = "2021") AND 
+    (year(start_date) <= 2021 AND
+    year(end_date) >= 2021) AND 
+    year(stream_date) <> "2021"
+
+# Write your MySQL query statement below
+
+SELECT
+    COUNT(distinct account_id) accounts_count
+FROM
+    Subscriptions su
+INNER JOIN
+    Streams st
+USING
+    (account_id)
+WHERE
+    (2021 BETWEEN year(start_date) AND year(end_date)) AND 
     year(stream_date) <> "2021"
