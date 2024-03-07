f55df4b94ee6cf18d15b92b0127b18df918487e4
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-07 12:38:05 -0800

    Create code.sql

---
+++ my-solutions/2738. Count Occurrences in Text/code.sql
@@ -0,0 +1,35 @@
+# Write your MySQL query statement below
+
+SELECT
+    "bull" word,
+    SUM(IF(content LIKE "% bull %", 1, 0)) count
+FROM
+    Files
+
+UNION
+
+SELECT
+    "bear" word,
+    SUM(IF(content LIKE "% bear %", 1, 0)) count
+FROM
+    Files
+
+# Write your MySQL query statement below
+
+SELECT
+    "bull" word,
+    COUNT(*) count
+FROM
+    Files
+WHERE
+    content LIKE "% bull %"
+
+UNION
+
+SELECT
+    "bear" word,
+    COUNT(*) count
+FROM
+    Files
+WHERE
+    content LIKE "% bear %"
