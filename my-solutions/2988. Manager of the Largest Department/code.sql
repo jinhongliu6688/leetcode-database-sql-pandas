ba1809f2ea78efb333fd2f137e0f684f22adb955
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-08 04:40:44 -0800

    Update code.sql

--- my-solutions/2988. Manager of the Largest Department/code.sql
+++ my-solutions/2988. Manager of the Largest Department/code.sql
@@ -34,3 +34,31 @@ WHERE
     position = "Manager"
 ORDER BY
     dep_id;
+
+# Write your MySQL query statement below
+
+WITH cte AS (
+    SELECT
+        dep_id
+    FROM (
+        SELECT
+            dep_id,
+            rank() over(order by COUNT(*) desc) rnk
+        FROM
+            Employees
+        GROUP BY
+            dep_id
+    ) temp_1
+    WHERE
+        rnk = 1
+)
+
+SELECT
+    emp_name manager_name,
+    dep_id
+FROM
+    Employees 
+WHERE
+    position = "Manager" AND dep_id IN (SELECT dep_id FROM cte)
+ORDER BY
+    dep_id;
