fd7163ee26351de124dc7c433e5ed30b6e7825c0
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-08 05:00:08 -0800

    Create code.sql

---
+++ my-solutions/2989. Class Performance/code.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+
+WITH cte As (
+    SELECT
+        DISTINCT assignment1 + assignment2 + assignment3 total,
+        rank() over(order by assignment1 + assignment2 + assignment3) rnk_a,
+        rank() over(order by assignment1 + assignment2 + assignment3 desc) rnk_d
+    FROM
+        Scores
+)
+
+SELECT
+    total - (
+        SELECT
+            total
+        FROM
+            cte
+        WHERE
+            rnk_a = 1
+    ) AS difference_in_score
+FROM
+    cte
+WHERE
+    rnk_d = 1;
