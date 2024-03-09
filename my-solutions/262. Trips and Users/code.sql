20d6d9e578e2795b70486cb7b6237bcb40fe9f5b
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-09 03:51:02 -0800

    Update code.sql

--- my-solutions/262. Trips and Users/code.sql
+++ my-solutions/262. Trips and Users/code.sql
@@ -30,3 +30,29 @@ WHERE
     request_at BETWEEN "2013-10-01" AND "2013-10-03"
 GROUP BY
     request_at;
+
+# Write your MySQL query statement below
+
+SELECT
+    request_at Day,
+    ROUND(AVG(status LIKE "cancelled%"), 2) "Cancellation Rate"
+FROM (
+    SELECT
+        *,
+        IF(AVG(banned = "No") = 1, 1, 0) "tag"
+    FROM
+        Trips t
+    INNER JOIN
+        Users u
+    ON
+        t.client_id = u.users_id OR
+        t.driver_id = u.users_id
+    WHERE
+        request_at BETWEEN "2013-10-01" AND "2013-10-03"
+    GROUP BY
+        id
+) temp
+WHERE
+    tag = 1
+GROUP BY
+    request_at
