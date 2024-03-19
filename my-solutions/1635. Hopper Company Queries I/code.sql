a29ffb2f27c05e3dcbe9b66e2e9ab513c71ae7ee
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-19 16:12:39 -0700

    Create code.sql

---
+++ my-solutions/1635. Hopper Company Queries I/code.sql
@@ -0,0 +1,62 @@
+# Write your MySQL query statement below
+
+with recursive cte AS (
+    select
+        1 as month
+ 
+    UNION ALL
+ 
+    select
+        month + 1 as month
+    from
+        cte
+    where
+        month < 12
+),
+accept as (
+    select
+        month(requested_at) month,
+        count(*) accepted_rides
+    from
+        Rides r
+    inner join
+        AcceptedRides a
+    using
+        (ride_id)
+    where
+        left(requested_at, 4) = 2020
+    group by
+        1
+)
+
+select
+    month,
+    ifnull(active_drivers, 0) active_drivers,
+    ifnull(accepted_rides, 0) accepted_rides
+from (
+    select
+        cte.month,
+        count(*) active_drivers
+    from cte, (
+        select
+            if(year(join_date) < 2020, 0, month(join_date)) as month 
+        from
+            Drivers
+        where
+            left(join_date, 4) <= 2020
+    ) temp
+    where
+        cte.month >= temp.month
+    group by
+        1
+    order by
+        1
+) temp_1
+left join
+    accept
+using
+    (month)
+right join
+    cte
+using
+    (month)
