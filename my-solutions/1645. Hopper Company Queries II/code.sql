4fcab675ceb6757d4742647f77ac3ce43de5d948
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-19 22:39:27 -0700

    Create code.sql

---
+++ my-solutions/1645. Hopper Company Queries II/code.sql
@@ -0,0 +1,63 @@
+# Write your MySQL query statement below
+
+with recursive cte AS (
+    select
+        1 as month
+ 
+    UNION ALL
+ 
+    select
+        month + 1 as month
+    from
+        cte
+    where
+        month < 12
+),
+accept as (
+    select
+        month(requested_at) month,
+        count(distinct driver_id) accepted_riders
+    from
+        Rides r
+    inner join
+        AcceptedRides a
+    using
+        (ride_id)
+    where
+        left(requested_at, 4) = 2020
+    group by
+        1
+),
+d_active as (
+    select
+        cte.month,
+        count(*) active_drivers
+    from cte, (
+        select
+            if(year(join_date) < 2020, 0, month(join_date)) as month 
+        from
+            Drivers
+        where
+            left(join_date, 4) <= 2020
+    ) temp
+    where
+        cte.month >= temp.month
+    group by
+        1
+    order by
+        1
+)
+
+select
+    month,
+    ifnull(round(ifnull(accepted_riders, 0) / active_drivers * 100, 2), 0) working_percentage
+from 
+    d_active
+left join
+    accept
+using
+    (month)
+right join
+    cte
+using
+    (month)
